87e1f42a2ca2a70957335bcfcd1f0eb4f16d56de
TangXingmeng <zhudms@163.com>
2017-09-09 11:36:12 +0800

    offical-demo
    注意各种配置文件

---
+++ openCVSamplefacedetection/.externalNativeBuild/ndkBuild/debug/obj/local/armeabi-v7a/objs/detection_based_tracker/DetectionBasedTracker_jni.o.d
@@ -0,0 +1,131 @@
+/Users/rongyile/Demo/jniDemo/face-detection4/openCVSamplefacedetection/.externalNativeBuild/ndkBuild/debug/obj/local/armeabi-v7a/objs/detection_based_tracker/DetectionBasedTracker_jni.o: \
+  /Users/rongyile/Demo/jniDemo/face-detection4/openCVSamplefacedetection/src/main/jni/DetectionBasedTracker_jni.cpp \
+  /Users/rongyile/Demo/jniDemo/face-detection4/openCVSamplefacedetection/src/main/jni/DetectionBasedTracker_jni.h \
+  /Users/rongyile/aboutOpenCV/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core.hpp \
+  /Users/rongyile/aboutOpenCV/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvdef.h \
+  /Users/rongyile/aboutOpenCV/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/hal/interface.h \
+  /Users/rongyile/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cstddef \
+  /Users/rongyile/aboutOpenCV/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/version.hpp \
+  /Users/rongyile/aboutOpenCV/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/base.hpp \
+  /Users/rongyile/aboutOpenCV/OpenCV-android-sdk/sdk/native/jni/include/opencv2/opencv_modules.hpp \
+  /Users/rongyile/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/climits \
+  /Users/rongyile/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/algorithm \
+  /Users/rongyile/aboutOpenCV/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvstd.hpp \
+  /Users/rongyile/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cstring \
+  /Users/rongyile/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cctype \
+  /Users/rongyile/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/string \
+  /Users/rongyile/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cstdlib \
+  /Users/rongyile/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cmath \
+  /Users/rongyile/aboutOpenCV/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/ptr.inl.hpp \
+  /Users/rongyile/aboutOpenCV/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/neon_utils.hpp \
+  /Users/rongyile/aboutOpenCV/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/traits.hpp \
+  /Users/rongyile/aboutOpenCV/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/matx.hpp \
+  /Users/rongyile/aboutOpenCV/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/saturate.hpp \
+  /Users/rongyile/aboutOpenCV/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/fast_math.hpp \
+  /Users/rongyile/aboutOpenCV/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/types.hpp \
+  /Users/rongyile/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cfloat \
+  /Users/rongyile/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/vector \
+  /Users/rongyile/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/limits \
+  /Users/rongyile/aboutOpenCV/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/mat.hpp \
+  /Users/rongyile/aboutOpenCV/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/bufferpool.hpp \
+  /Users/rongyile/aboutOpenCV/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/mat.inl.hpp \
+  /Users/rongyile/aboutOpenCV/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/persistence.hpp \
+  /Users/rongyile/aboutOpenCV/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/operations.hpp \
+  /Users/rongyile/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cstdio \
+  /Users/rongyile/aboutOpenCV/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvstd.inl.hpp \
+  /Users/rongyile/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/complex \
+  /Users/rongyile/aboutOpenCV/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/utility.hpp \
+  /Users/rongyile/aboutOpenCV/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/core_c.h \
+  /Users/rongyile/aboutOpenCV/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/types_c.h \
+  /Users/rongyile/aboutOpenCV/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/optim.hpp \
+  /Users/rongyile/aboutOpenCV/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/ovx.hpp \
+  /Users/rongyile/aboutOpenCV/OpenCV-android-sdk/sdk/native/jni/include/opencv2/objdetect.hpp \
+  /Users/rongyile/aboutOpenCV/OpenCV-android-sdk/sdk/native/jni/include/opencv2/objdetect/detection_based_tracker.hpp \
+  /Users/rongyile/aboutOpenCV/OpenCV-android-sdk/sdk/native/jni/include/opencv2/objdetect/objdetect_c.h \
+  /Users/rongyile/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/deque
+
+/Users/rongyile/Demo/jniDemo/face-detection4/openCVSamplefacedetection/src/main/jni/DetectionBasedTracker_jni.h:
+
+/Users/rongyile/aboutOpenCV/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core.hpp:
+
+/Users/rongyile/aboutOpenCV/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvdef.h:
+
+/Users/rongyile/aboutOpenCV/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/hal/interface.h:
+
+/Users/rongyile/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cstddef:
+
+/Users/rongyile/aboutOpenCV/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/version.hpp:
+
+/Users/rongyile/aboutOpenCV/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/base.hpp:
+
+/Users/rongyile/aboutOpenCV/OpenCV-android-sdk/sdk/native/jni/include/opencv2/opencv_modules.hpp:
+
+/Users/rongyile/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/climits:
+
+/Users/rongyile/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/algorithm:
+
+/Users/rongyile/aboutOpenCV/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvstd.hpp:
+
+/Users/rongyile/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cstring:
+
+/Users/rongyile/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cctype:
+
+/Users/rongyile/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/string:
+
+/Users/rongyile/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cstdlib:
+
+/Users/rongyile/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cmath:
+
+/Users/rongyile/aboutOpenCV/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/ptr.inl.hpp:
+
+/Users/rongyile/aboutOpenCV/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/neon_utils.hpp:
+
+/Users/rongyile/aboutOpenCV/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/traits.hpp:
+
+/Users/rongyile/aboutOpenCV/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/matx.hpp:
+
+/Users/rongyile/aboutOpenCV/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/saturate.hpp:
+
+/Users/rongyile/aboutOpenCV/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/fast_math.hpp:
+
+/Users/rongyile/aboutOpenCV/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/types.hpp:
+
+/Users/rongyile/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cfloat:
+
+/Users/rongyile/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/vector:
+
+/Users/rongyile/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/limits:
+
+/Users/rongyile/aboutOpenCV/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/mat.hpp:
+
+/Users/rongyile/aboutOpenCV/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/bufferpool.hpp:
+
+/Users/rongyile/aboutOpenCV/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/mat.inl.hpp:
+
+/Users/rongyile/aboutOpenCV/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/persistence.hpp:
+
+/Users/rongyile/aboutOpenCV/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/operations.hpp:
+
+/Users/rongyile/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cstdio:
+
+/Users/rongyile/aboutOpenCV/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvstd.inl.hpp:
+
+/Users/rongyile/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/complex:
+
+/Users/rongyile/aboutOpenCV/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/utility.hpp:
+
+/Users/rongyile/aboutOpenCV/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/core_c.h:
+
+/Users/rongyile/aboutOpenCV/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/types_c.h:
+
+/Users/rongyile/aboutOpenCV/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/optim.hpp:
+
+/Users/rongyile/aboutOpenCV/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/ovx.hpp:
+
+/Users/rongyile/aboutOpenCV/OpenCV-android-sdk/sdk/native/jni/include/opencv2/objdetect.hpp:
+
+/Users/rongyile/aboutOpenCV/OpenCV-android-sdk/sdk/native/jni/include/opencv2/objdetect/detection_based_tracker.hpp:
+
+/Users/rongyile/aboutOpenCV/OpenCV-android-sdk/sdk/native/jni/include/opencv2/objdetect/objdetect_c.h:
+
+/Users/rongyile/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/deque:
